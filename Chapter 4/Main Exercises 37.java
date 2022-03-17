ce1cf1e31f561d498edbc0c2addc6c7a691d781a
Eusebiu <55321643+mrbseba@users.noreply.github.com>
2022-03-17 14:47:21 +0200

    Update Main Exercises 37.java

--- Chapter 4/Main Exercises 37.java
+++ Chapter 4/Main Exercises 37.java
@@ -9,20 +9,20 @@ package second_try;
 
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         long i = 1, nr_fact_calc = 0, fact = 1;
-        double j = 0, e = 0,  e_sqrt = 0;
-        System.out.print("Introduce numarul pentru calcularea factorialului: ");
+        double j = 0, e = 0,  e_sqrt = 0, putear_unu = 2;
+                System.out.print("Introduce numarul pentru calcularea factorialului: ");
         nr_fact_calc = input.nextInt();
         //calcularea de factorial
         for (i = 1; i < nr_fact_calc + 1; i++) {
             fact = fact * i;
             e += 1 + 1/fact;
-            e_sqrt = sqrt(e);
+            e_sqrt = Math.pow(e, putear_unu);
         }
         System.out.printf(" e = %.2f\n", e);
         System.out.printf(" e^x = %.2f\n", e_sqrt);
