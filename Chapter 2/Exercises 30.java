3974eebb91dde89eda1c7998b494b9643fbc7084
Eusebiu <55321643+mrbseba@users.noreply.github.com>
2022-03-21 13:36:40 +0200

    Add files via upload

--- Chapter 2/Exercises 30.java
+++ Chapter 2/Exercises 30.java
@@ -1,14 +1,8 @@
 //Chapter 6, exe 21
 /*
-(Separating Digits) Write methods that accomplish each of the following tasks:
-a) Calculate the integer part of the quotient when integer a is divided by integer b.
-b) Calculate the integer remainder when integer a is divided by integer
-c) Use the methods developed in parts (a) and (b) to write a method displayDigits that receives an integer
-between 1 and 99999 and displays it as a sequence of digits, separating
-each pair of digits by two spaces. For example, the integer 4562 should appear as
-4 5 6 2
-Incorporate the methods into an application that inputs an integer and calls displayDigits by passing the method
-the integer entered. Display the results.
+(Separating the Digits in an Integer) Write an application that inputs one number consisting of five digits from the user, separates the number into its individual digits and prints the digits
+separated from one another by three spaces each. For example, if the user types in the number 42339,
+the program should print
 * */
 package second_try;
 
